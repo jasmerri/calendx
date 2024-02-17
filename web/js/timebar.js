1b15e7773313065bdb43d08171feea3d75bacec0
jasmerri <jasmine.erringer@gmail.com>
2024-02-17 13:49:05 -0800

    use css grid

---
+++ web/js/timebar.js
@@ -0,0 +1,14 @@
+class Timebar {
+    constructor(element) {
+        let context = element.getContext("2d");
+        new ResizeObserver
+    }
+
+    resize(element, width, height) {
+
+    }
+
+    render(context, width, height) {
+        context.fillRect(0, 0, width, height);
+    }
+}
