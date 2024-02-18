90d24f959a71dda7bf31503790e9bff4e3caf59c
jasmerri <jasmine.erringer@gmail.com>
2024-02-18 09:23:08 -0800

    remove example events

--- web/js/main.js
+++ web/js/main.js
@@ -1,21 +1,11 @@
 import { Timebar } from "./timebar.js";
 import { Schedule } from "./schedule/schedule.js";
-import { createWeeklyEntry } from "./schedule/helpers.js";
-import { addDays, justDay } from "./schedule/dateutil.js";
-import { createRange } from "./schedule/range.js";
 import { makeDays, SceneManager } from "./scene.js";
 import { correctMonthFormat, displayMonthEvents } from "./scene.js";
 import { Editor } from "./editor.js";
 import { WeekView } from "./weekview.js";
 
 let schedule = new Schedule();
-
-let sample = createWeeklyEntry("cook", "#ffabcd", addDays(justDay(Date.now()), -1), [ true, true, true, true, true, true, true ], createRange(0, 1800000));
-schedule.registerEntry(sample);
-let sample3 = createWeeklyEntry("elizabeth", "#a032f7", addDays(justDay(Date.now()), -1), [ true, true, true, true, true, true, true ], createRange(9000000, 27000000));
-schedule.registerEntry(sample3);
-let sample4 = createWeeklyEntry("[", "#ff0000", addDays(justDay(Date.now()), -1), [ true, true, true, true, true, true, true ], createRange(11000000, 36000000));
-schedule.registerEntry(sample4);
 
 let timebar = new Timebar(document.querySelector("#time-bar"), document.querySelector("#time-bar-container"), schedule);
 let weekview = new WeekView(schedule, timebar);
