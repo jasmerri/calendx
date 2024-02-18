a2be446a2b8ec62eba0d06ee2d1c08b3602c2011
calebh13 <caleb.hansen1@wsu.edu>
2024-02-18 01:43:46 -0800

    format month calendar properly

--- web/js/scene.js
+++ web/js/scene.js
@@ -15,15 +15,37 @@ export function makeDays() {
     for(let i = 1; i <= 42; i++) { // loop for month days
         let dynamicId = "day" + i + "m";
         let day = document.createElement("div");
-        let text = document.createTextNode("Day " + i);
+        let text = document.createTextNode(i);
         day.appendChild(text);
+        day.classList.add("day");
         document.querySelector("#month-calendar").appendChild(day).id = dynamicId;
     }
     for(let i = 1; i <= 7; i++) { // loop for week days
         let dynamicId = "day" + i + "w";
         let day = document.createElement("div");
-        let text = document.createTextNode("Day " + i);
+        let text = document.createTextNode(i);
         day.appendChild(text);
+        day.classList.add("day");
         document.querySelector("#week-calendar").appendChild(day).id = dynamicId;
     }
+}
+
+export function correctMonthFormat() {
+    let currentDay = new Date();
+    currentDay.setDate(1); // change to first day of the month
+    const dayNum = currentDay.getDay();
+    // potential feature: don't remove previous month's days, but update their numbers as well
+    // and gray them out instead of making invisible
+    let i = 1;
+    for(; i <= dayNum; i++) { // Remove previous month's days
+        document.querySelector("#day" + i + "m").classList.add("invisible");
+    }
+    let daysInMonth = new Date(currentDay.getFullYear(), currentDay.getMonth() + 1, 0).getDate();
+    for(i--; i <= daysInMonth + dayNum; i++) { // Change displayed dates of current month's days
+        document.querySelector("#day" + i + "m").innerHTML = (i - dayNum);
+    }
+    for(; i <= 42; i++) { // Remove next month's days
+        document.querySelector("#day" + i + "m").style.display = "none";
+    }
+    return dayNum; // return the id of the actual first day
 }
